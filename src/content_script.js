399c012d04900e7a33261a03edd1349008215a17
Guy Langford-Lee <guy@langford-lee.com>
2015-11-27 14:39:02 +1300

    inital commit

---
+++ src/content_script.js
@@ -0,0 +1,43 @@
+var mainLoopId = setInterval(function(){
+    // Do your update stuff...
+    walk(document.body);
+}, 40);
+function walk(node) 
+{
+	// I stole this function from here:
+	// http://is.gd/mwZp7E
+	
+	var child, next;
+
+	switch ( node.nodeType )  
+	{
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while ( child ) 
+			{
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+
+		case 3: // Text node
+			handleText(node);
+			break;
+	}
+}
+
+function handleText(textNode) 
+{
+	var v = textNode.nodeValue;
+
+	v = v.replace(/(the\s)\b\S+\b/, "$1penis")
+	   v = v.replace(/(The\s)\b\S+\b/, "$1penis")
+
+
+	textNode.nodeValue = v;
+}
+
+
